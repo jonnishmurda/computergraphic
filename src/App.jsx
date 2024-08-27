052b04c9a4e2fd3aa0be59476a7911d7d849d1e8
jonnishmurda <jjo1@hi.is>
2024-08-27 12:54:49 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Nav from './components/Nav'
 
 function App() {
-  
+
 
   return (
     <main>
-
+      <Nav />
     </main>
   )
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Nav() {
+    return (
+        <div>
+            <h1>TÖL105G - Tölvugrafík Verkefni</h1>
+            <h2>Jonathan Jakub Otuoma - <a href="mailto:jjo1@hi.is">jjo1@hi.is</a></h2>
+        </div>
+    )
+}
+
+export default Nav

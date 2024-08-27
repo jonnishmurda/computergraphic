bc200da89da5d5cc07e7f1aea044492c75b6ea29
jonnishmurda <jjo1@hi.is>
2024-08-27 12:59:27 +0000

    commit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import style from './Nav.module.css'
 
 function Nav() {
     return (
-        <div>
-            <h1>TÖL105G - Tölvugrafík Verkefni</h1>
-            <h2>Jonathan Jakub Otuoma - <a href="mailto:jjo1@hi.is">jjo1@hi.is</a></h2>
-        </div>
+        <header>
+            <div>
+                <h1>TÖL105G - Tölvugrafík Verkefni</h1>
+                <h2>Jonathan Jakub Otuoma - <a href="mailto:jjo1@hi.is">jjo1@hi.is</a></h2>
+            </div>
+
+            <div className={style.dropdown}>
+                <button className={style.dropbtn}>Dropdown</button>
+                <div className={style.dropdownContent}>
+                    <Link to="#">Link 1</Link>
+                    <Link to="#">Link 2</Link>
+                    <Link to="#">Link 3</Link>
+                </div>
+            </div>
+
+        </header>
+
     )
 }
 

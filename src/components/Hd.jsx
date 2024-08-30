c326bc399a6c650d410eb450a035ae922108453b
jonnishmurda <jjo1@hi.is>
2024-08-30 11:09:55 +0000

    commit

---
+++ src/components/Hd.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hd() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Hd
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,12 +9,18 @@ function Nav() {
                 <h1>TÖL105G - Tölvugrafík Verkefni</h1>
                 <h2>Jonathan Jakub Otuoma - <a href="mailto:jjo1@hi.is">jjo1@hi.is</a></h2>
             </div>
-            <div className={style.dropdown}>
-                <button className={style.dropbtn}>Verkefni</button>
-                <div className={style.dropdownContent}>
-                    <Link to="#">Verkefni 1</Link>
-                    <Link to="#">Verkefni 2</Link>
-                    <Link to="#">Verkefni 3</Link>
+            <div className={style.buttons}>
+                <div className={style.dropdown}>
+                    <button className={style.dropbtn}>Verkefni</button>
+                    <div className={style.dropdownContent}>
+                        <Link to="#">Verkefni 1</Link>
+                    </div>
+                </div>
+                <div className={style.dropdown}>
+                    <button className={style.dropbtn}>Heimadæmi</button>
+                    <div className={style.dropdownContent}>
+                        <Link to="#">Heimadæmi 2</Link>
+                    </div>
                 </div>
             </div>
 
